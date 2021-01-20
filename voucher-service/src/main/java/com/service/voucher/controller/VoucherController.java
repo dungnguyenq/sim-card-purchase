528015807c1ab4cd0a1835b184e07c17bc01929e
Dung Quoc Nguyen <quocdunguit08@gmail.com>
2021-01-20 23:52:25 +0700

    Refactor - change name getvoucher to avoid confuse, produces to return application/json

--- voucher-service/src/main/java/com/service/voucher/controller/VoucherController.java
+++ voucher-service/src/main/java/com/service/voucher/controller/VoucherController.java
@@ -6,11 +6,14 @@ import com.service.voucher.service.VoucherService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -21,17 +24,18 @@ public class VoucherController {
 
     final static Logger logger = LogManager.getLogger(VoucherController.class);
 
-    @PostMapping("/")
-    public ResponseEntity<?> getVoucher(@RequestBody PhoneDto phoneDto){
+    @PostMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> generateVoucher(@RequestBody PhoneDto phoneDto){
         VoucherDto voucherDto = voucherService.getVoucher(phoneDto.getPhoneNumber());
+
         if (voucherDto == null){
-            return new ResponseEntity("Voucher code will be sent through SMS later", HttpStatus.SERVICE_UNAVAILABLE);
+            return new ResponseEntity(Collections.singletonMap("message", "Voucher code will be sent through SMS later"), HttpStatus.SERVICE_UNAVAILABLE);
         }
 
         return new ResponseEntity(voucherDto, HttpStatus.CREATED);
     }
 
-    @GetMapping("/{phone-number}")
+    @GetMapping(value = "/{phone-number}", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<VoucherDto> getVouchers(@PathVariable("phone-number") String phoneNumber){
         List<VoucherDto> results = new ArrayList<>();
         try{
