ab2d74c44a620f33d1ce062d85166fdc6a168d1c
Dung Quoc Nguyen <quocdunguit08@gmail.com>
2021-01-23 15:14:55 +0700

    Refactor - update logic generate voucher code of 3rd party

--- third-party-server/src/main/java/com/thirdparty/service/ThirdPartyServiceImpl.java
+++ third-party-server/src/main/java/com/thirdparty/service/ThirdPartyServiceImpl.java
@@ -23,7 +23,7 @@ public class ThirdPartyServiceImpl implements ThirdPartyService {
     @Override
     public String getVoucherCode() throws Exception {
         // set random time to delay request
-        List<Integer> timeDelays = Arrays.asList(3000, 4000, 8000, 10000, 15000, 29000, 30000, 31000, 35000, 40000, 60000, 80000, 120000, 125000);
+        List<Integer> timeDelays = Arrays.asList(0, 3000, 4000, 8000, 10000, 15000, 29000, 30000, 31000, 35000, 40000, 60000, 80000, 120000, 125000);
         Random rand = new Random();
         int randomTimeDelay = timeDelays.get(rand.nextInt(timeDelays.size()));
         System.out.println("Delay Time: " + randomTimeDelay);
@@ -32,7 +32,7 @@ public class ThirdPartyServiceImpl implements ThirdPartyService {
         Random random = new Random();
 
         String newVoucher = random.ints(leftLimit, rightLimit + 1)
-                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+                .filter(i -> (i <= 57 || (i >= 65 && i <= 90) || i >= 97))
                 .limit(voucherLength)
                 .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
                 .toString();
